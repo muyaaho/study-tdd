588149077035fb7ad6c7c4253ffaed4bf7dcb36f
muyaaho <skanthsu715@gmail.com>
2023-11-05 22:24:32 +0900

    feat: 10만원 지불에 대한 처리 추가

--- src/test/java/chap03/ExpirtDateCalculatorTest.java
+++ src/test/java/chap03/ExpirtDateCalculatorTest.java
@@ -95,6 +95,15 @@ public class ExpirtDateCalculatorTest {
                         .payAmount(30_000).build(), LocalDate.of(2019, 7, 31));
     }
 
+    @Test
+    void 십만원을_납부하면_1년_제공() {
+        assertExpiryDate(PayData.builder()
+                        .billingDate(LocalDate.of(2019, 1, 28))
+                        .payAmount(100_000)
+                        .build(),
+                LocalDate.of(2020, 1, 28));
+    }
+
     private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
         ExpiryDateCalculator cal = new ExpiryDateCalculator();
         LocalDate realExpiryDate = cal.calculateExpiryDate(payData);
