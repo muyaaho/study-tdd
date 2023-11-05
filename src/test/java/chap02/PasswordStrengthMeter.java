f8d3e65a98d5691213cd85245301deb23ccb97c4
muyaaho <skanthsu715@gmail.com>
2023-11-05 20:28:18 +0900

    feat: 길이가 8글자 이상인 조건만 충족하는 경우 검증 처리 추가

--- src/test/java/chap02/PasswordStrengthMeter.java
+++ src/test/java/chap02/PasswordStrengthMeter.java
@@ -1,35 +1,46 @@
 package test.java.chap02;
 
 /* 테스트 할 때 단위기능 별로 하는데 처음에는 테스트가 통과되도록 만들고
-* 이제 여러 기능을 추가하다보면 한 메소드가 길어지는데 그 때 마다 private으로 나눠주면
-* 되는거임! 정말 세세한 기능 하나하나 테스트 할 필요 없음 우리는 기능목록을 테스트 하면
-* 됨! */
+ * 이제 여러 기능을 추가하다보면 한 메소드가 길어지는데 그 때 마다 private으로 나눠주면
+ * 되는거임! 정말 세세한 기능 하나하나 테스트 할 필요 없음 우리는 기능목록을 테스트 하면
+ * 됨! */
 public class PasswordStrengthMeter {
 
-    public PasswordStrength meter(String s){
-        if (s==null||s.isEmpty()) return PasswordStrength.INVALID;
-        if (s.length() < 8){
+    public PasswordStrength meter(String s) {
+        if (s == null || s.isEmpty()) {
+            return PasswordStrength.INVALID;
+        }
+        boolean lengthEnough = s.length() >= 8;
+        boolean containsNum = meetsContainingNumberCriteria(s);
+        boolean containUpp = meetsContainingUppercaseCriteria(s);
+
+        if (lengthEnough && !containsNum && !containUpp) {
+            return PasswordStrength.WEAK;
+        }
+        if (!containsNum) {
             return PasswordStrength.NORMAL;
         }
-        boolean containsNum  = meetsContainingNumberCriteria(s);
-        if (!containsNum) return PasswordStrength.NORMAL;
-        boolean containUpp = meetsContainingUppercaseCriteria(s);
-        if (!containUpp) return PasswordStrength.NORMAL;
+        if (!lengthEnough) {
+            return PasswordStrength.NORMAL;
+        }
+        if (!containUpp) {
+            return PasswordStrength.NORMAL;
+        }
         return PasswordStrength.STRONG;
     }
 
-    private boolean meetsContainingNumberCriteria(String s){
-        for (char ch: s.toCharArray()){
-            if (ch >= '0' && ch <= '9'){
+    private boolean meetsContainingNumberCriteria(String s) {
+        for (char ch : s.toCharArray()) {
+            if (ch >= '0' && ch <= '9') {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean meetsContainingUppercaseCriteria(String s){
-        for (char ch: s.toCharArray()){
-            if(Character.isUpperCase(ch)){
+    private boolean meetsContainingUppercaseCriteria(String s) {
+        for (char ch : s.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
                 return true;
             }
         }
--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -10,41 +10,46 @@ public class PasswordStrengthMeterTest {
 
     // 중복 제거하기 위해 추가 2
     /* 무턱대고 제거하면 안 된다. 중복을 제거한 뒤에도 테스트 코드의 가독성이 떨어지지 않고 수정이 용이한 경우에만 중복을 제거해야 한다.*/
-    private void assertStrength(String password, PasswordStrength expStr){
+    private void assertStrength(String password, PasswordStrength expStr) {
         PasswordStrength result = meter.meter(password);
         assertEquals(expStr, result);
     }
+
     @Test
-    void meetsAllCriteria_Then_String(){
+    void meetsAllCriteria_Then_String() {
         assertStrength("ab12!@AB", PasswordStrength.STRONG);
         assertStrength("abc1!Add", PasswordStrength.STRONG);
     }
 
     @Test
-    void meetsOtherCriteria_except_for_Length_Then_Normal(){
+    void meetsOtherCriteria_except_for_Length_Then_Normal() {
         assertStrength("ab12!@A", PasswordStrength.NORMAL);
         assertStrength("Ab12!c", PasswordStrength.NORMAL);
     }
 
     @Test
-    void meetsOtherCriteria_except_for_number_Then_Normal(){
+    void meetsOtherCriteria_except_for_number_Then_Normal() {
         assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
 
     }
 
     @Test
-    void nullInput_Then_Invalid(){
+    void nullInput_Then_Invalid() {
         assertStrength(null, PasswordStrength.INVALID);
     }
 
     @Test
-    void emptyInput_Then_Invalid(){
+    void emptyInput_Then_Invalid() {
         assertStrength("", PasswordStrength.INVALID);
     }
 
     @Test
-    void meetsOtherCriteria_except_for_Uppercase_Then_Normal(){
+    void meetsOtherCriteria_except_for_Uppercase_Then_Normal() {
         assertStrength("ab12!@df", PasswordStrength.NORMAL);
     }
 
+    @Test
+    void meetsOnlyLengthCriteria_Then_Weak() {
+        assertStrength("abdefghi", PasswordStrength.WEAK);
+    }
 }
