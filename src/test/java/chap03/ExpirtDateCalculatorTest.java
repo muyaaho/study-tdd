350bdb1d27f886aeee70fb38a3d7b410c76c01d1
muyaaho <skanthsu715@gmail.com>
2023-11-05 22:00:43 +0900

    feat: 첫 납부일과 만료일 일자가 다를 때 만원 납부하는 경우 추가

--- src/test/java/chap03/ExpirtDateCalculatorTest.java
+++ src/test/java/chap03/ExpirtDateCalculatorTest.java
@@ -38,8 +38,28 @@ public class ExpirtDateCalculatorTest {
                         .payAmount(10_000)
                         .build(),
                 LocalDate.of(2020, 2, 29));
+    }
 
+    @Test
+    void 첫_납부일과_만료일_일자가_다를때_만원_납부() {
+        PayData payData = PayData.builder().firstBillingDate(LocalDate.of(2019, 1, 31))
+                .billingDate(LocalDate.of(2019, 2, 28)).payAmount(10_000).build();
+        assertExpiryDate(payData, LocalDate.of(2019, 3, 31));
 
+        PayData payData2 = PayData.builder()
+                .firstBillingDate(LocalDate.of(2019, 1, 30))
+                .billingDate(LocalDate.of(2019, 2, 28))
+                .payAmount(10_000)
+                .build();
+        assertExpiryDate(payData2, LocalDate.of(2019, 3, 30));
+
+        // 첫 납부일이 3019-05-31이고 만료되는 2019-06-30에 1만원을 납부하면 다음 만료일은 2019-07-31이다.
+        PayData payData3 = PayData.builder()
+                .firstBillingDate(LocalDate.of(2019, 5, 31))
+                .billingDate(LocalDate.of(2019, 6, 30))
+                .payAmount(10_000)
+                .build();
+        assertExpiryDate(payData3, LocalDate.of(2019, 7, 31));
     }
 
     private void assertExpiryDate(PayData payData, LocalDate expectedExpiryDate) {
--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -4,6 +4,15 @@ import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
+        if (payData.getFirstBillingDate() != null) {
+            // 후보 만료일 구함
+            LocalDate candidateExp = payData.getBillingDate().plusMonths(1);
+            // 첫 납부일의 일자와 후보 만료일의 일자가 다르면
+            if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
+                // 첫 납부일의 일자를 후보 만료일의 일자로 사용
+                return candidateExp.withDayOfMonth(payData.getFirstBillingDate().getDayOfMonth());
+            }
+        }
         return payData.getBillingDate().plusMonths(1);
     }
 
