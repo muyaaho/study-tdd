3c16edbe9bc3882bf4f51ff0ef424312b28bfddd
muyaaho <skanthsu715@gmail.com>
2023-11-05 20:39:23 +0900

    refactor: 중복 코드 개선

--- src/test/java/chap02/PasswordStrengthMeter.java
+++ src/test/java/chap02/PasswordStrengthMeter.java
@@ -10,28 +10,24 @@ public class PasswordStrengthMeter {
         if (s == null || s.isEmpty()) {
             return PasswordStrength.INVALID;
         }
-        boolean lengthEnough = s.length() >= 8;
-        boolean containsNum = meetsContainingNumberCriteria(s);
-        boolean containsUpp = meetsContainingUppercaseCriteria(s);
+        int metCounts = 0;
 
-        if (lengthEnough && !containsNum && !containsUpp) {
+        if (s.length() >= 8) {
+            metCounts++;
+        }
+        if (meetsContainingNumberCriteria(s)) {
+            metCounts++;
+        }
+        if (meetsContainingUppercaseCriteria(s)) {
+            metCounts++;
+        }
+        if (metCounts == 1) {
             return PasswordStrength.WEAK;
         }
-        if (!lengthEnough && containsNum && !containsUpp) {
-            return PasswordStrength.WEAK;
-        }
-        if (!lengthEnough && !containsNum && containsUpp) {
-            return PasswordStrength.WEAK;
-        }
-        if (!containsNum) {
+        if (metCounts == 2) {
             return PasswordStrength.NORMAL;
         }
-        if (!lengthEnough) {
-            return PasswordStrength.NORMAL;
-        }
-        if (!containsUpp) {
-            return PasswordStrength.NORMAL;
-        }
+
         return PasswordStrength.STRONG;
     }
 
